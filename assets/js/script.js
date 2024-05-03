37764fdccadb8ef42e1a00f2247348119aa0c024
GustavoNbs <nebesgustavo@gmail.com>
2024-05-03 03:56:33 -0300

    Arrumando Erros e Otimizando Codigo.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,40 +6,35 @@ document.addEventListener('DOMContentLoaded', function() {
     const portfolioBtn = document.getElementById('portfolioBtn');
     const conteudoPortfolio = document.getElementById('conteudoPortfolio');
     const contatoBtn = document.getElementById('contatoBtn');
-    let confirm = confirm('Você sera redirecionado ao Whatsapp. Deseja Prosseguir?');
 
     sobreMimBtn.addEventListener('click', function() {
         console.log("Clicou em Sobre Mim");
         conteudoSobreMim.style.display = 'block';
         conteudoPortfolio.style.display = 'none';
+        sobreMimBtn.classList.add('ativo');
+        portfolioBtn.classList.remove('ativo');
+        contatoBtn.classList.remove('ativo');
     });
-
 
     portfolioBtn.addEventListener('click', function() {
         console.log("Clicou em Portfolio");
         conteudoSobreMim.style.display = 'none';
         conteudoPortfolio.style.display = 'block';
+        sobreMimBtn.classList.remove('ativo');
+        portfolioBtn.classList.add('ativo');
+        contatoBtn.classList.remove('ativo');
+    });
+
+    contatoBtn.addEventListener('click', function() {
+        console.log("Clicou em Contato");
+        sobreMimBtn.classList.remove('ativo');
+        portfolioBtn.classList.remove('ativo');
+        contatoBtn.classList.add('ativo');
+        let confirmacao = confirm('Você será redirecionado ao WhatsApp. Deseja prosseguir?');
+        if (confirmacao) {
+            window.open('https://wa.me/5541999381544', '_blank');
+        }
     });
 });
 
-sobreMimBtn.addEventListener('click', function() {
-    sobreMimBtn.classList.add('ativo');
-    portfolioBtn.classList.remove('ativo');
-    contatoBtn.classList.remove('ativo');
-});
-
-portfolioBtn.addEventListener('click', function() {
-    sobreMimBtn.classList.remove('ativo');
-    contatoBtn.classList.remove('ativo');
-    portfolioBtn.classList.add('ativo');
-});
-
-contatoBtn.addEventListener('click',function() {
-    sobreMimBtn.classList.remove('ativo');
-    portfolioBtn.classList.remove('ativo');
-    contatoBtn.classList.add('ativo');
-    let confirmacao = confirm('Você será redirecionado ao WhatsApp. Deseja prosseguir?');
-        if (confirmacao) {
-            window.location.href = 'https://wa.me/5541999381544';
-}})
 
