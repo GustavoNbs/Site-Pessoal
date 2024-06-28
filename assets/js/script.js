f6ca2fb0aa65b5ddbd470435e45455d9455093d8
GustavoNbs <nebesgustavo@gmail.com>
2024-06-28 07:55:38 -0300

    Api Git Hub

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const username = 'GustavoNbs';
+
+fetch(`https://api.github.com/users/${username}/repos`)
+    .then(response => response.json())
+    .then(data => {
+        const repos = data;
+
+        repos.forEach(repo => {
+            const card = `
+                <div class="card text-bg-dark bordacard mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">${repo.name}</h5>
+                        <p class="card-text">${repo.description ? repo.description : 'Descrição não disponível'}</p>
+                        <button type="button" class="btn botaoteste" onclick="window.open('${repo.html_url}', '_blank');">
+                            Repositório
+                        </button>
+                        <button type="button" class="btn botaoteste" ${repo.homepage ? `onclick="window.open('${repo.homepage}', '_blank');"` : 'disabled'}>
+                            GitHub Pages
+                        </button>
+                    </div>
+                </div>
+            `;
+
+            document.getElementById('CardsAPI').innerHTML += card;
+        });
+    })
+    .catch(error => console.error('Erro:', error));
