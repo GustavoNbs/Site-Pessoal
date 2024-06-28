7487ade5bb6cd5be59466feb8fc92d5cf4326062
GustavoNbs <nebesgustavo@gmail.com>
2024-06-28 19:13:49 -0300

    Demo2

---
+++ assets/Demo2/assets/js/app.js
@@ -0,0 +1,143 @@
+window.onload = function() {
+    carregaLista()
+}
+
+function adicionaTarefaNaLista() {
+    const novaTarefa = document.getElementById('input_nova_tarefa').value.trim()
+
+    if (novaTarefa !== '') {
+        criaNovoItemDaLista(novaTarefa)
+        document.getElementById("input_nova_tarefa").value = ""
+        ajustarAlturaContainer()
+        salvaLista()
+    } else {
+        alert('Por favor, insira um texto para a tarefa.')
+    }
+}
+
+function criaNovoItemDaLista(textoDaTarefa) {
+    const listaTarefas = document.getElementById('lista_de_tarefas')
+
+    const novoItem = document.createElement('li')
+    
+    const idUnico = Date.now()
+
+    novoItem.innerText = textoDaTarefa
+    novoItem.id = `tarefa_id_${idUnico}`
+    novoItem.classList.add("ItemTarefa")
+
+    novoItem.appendChild(criaInputCheckBoxTarefa(novoItem.id))
+
+    novoItem.appendChild(criaBotaoEditarTarefa(novoItem.id))
+
+    novoItem.appendChild(criaBotaoApagarTarefa(novoItem.id))
+
+    listaTarefas.appendChild(novoItem)
+}
+
+function criaInputCheckBoxTarefa(idTarefa) {
+    const inputTarefa = document.createElement('input')
+    inputTarefa.type = 'checkbox'
+    inputTarefa.setAttribute('onclick', `mudaEstadoTarefa('${idTarefa}')`)
+    return inputTarefa
+}
+
+function mudaEstadoTarefa(idTarefa) {
+    const tarefaSelecionada = document.getElementById(idTarefa)
+    if (tarefaSelecionada.style.textDecoration == 'line-through') {
+        tarefaSelecionada.style = 'text-decoration: none;'
+    } else {
+        tarefaSelecionada.style = 'text-decoration: line-through;'
+    }    
+    salvaLista()
+}
+
+function criaBotaoEditarTarefa(idTarefa) {
+    const botaoEditar = document.createElement('button')
+    //botaoEditar.innerText = 'Editar'
+    botaoEditar.classList.add("BotaoEditar")
+    botaoEditar.setAttribute('onclick', `editarTarefa('${idTarefa}')`)
+    return botaoEditar
+}
+
+function criaBotaoApagarTarefa(idTarefa) {
+    const botaoApagar = document.createElement('button')
+    // botaoApagar.innerText = 'Apagar'
+    botaoApagar.classList.add("BotaoApagar")
+    botaoApagar.setAttribute('onclick', `apagarTarefa('${idTarefa}')`)
+    return botaoApagar
+}
+
+function editarTarefa(idTarefa) {
+    const textoAtual = document.getElementById(idTarefa).firstChild
+    const inputEdicao = document.createElement('input')
+    inputEdicao.type = 'text'
+    inputEdicao.value = textoAtual.textContent
+    inputEdicao.addEventListener('keyup', function(enter) {
+        if (enter.key === 'Enter') {
+            textoAtual.textContent = this.value
+            this.parentNode.replaceChild(textoAtual, this)
+            salvaLista()
+        }
+    })
+    textoAtual.parentNode.replaceChild(inputEdicao, textoAtual)
+}
+
+function apagarTarefa(idTarefa) {
+    const confirmacao = confirm('Tem certeza que deseja apagar esta tarefa?')
+    if (confirmacao) {
+        const apagaTexto = document.getElementById(idTarefa).parentNode
+        apagaTexto.removeChild(document.getElementById(idTarefa))
+        ajustarAlturaContainer()
+    }
+    salvaLista()
+}
+
+function ocultarTarefasMarcadas() {
+    const tarefasMarcadas = document.querySelectorAll('li[style*="line-through"]')
+    tarefasMarcadas.forEach(function(tarefa) {
+        tarefa.style.display = 'none'
+    })
+    ajustarAlturaContainer()
+    salvaLista()
+}
+
+function mostrarTarefasMarcadas() {
+    const tarefasMarcadas = document.querySelectorAll('li[style*="line-through"]')
+    tarefasMarcadas.forEach(function(tarefa) {
+        tarefa.style.display = 'list-item'
+    })
+    ajustarAlturaContainer()
+    salvaLista()
+}
+
+function apagarLista() {
+    const confirmacao = confirm('Tem certeza que deseja apagar toda a lista?')
+    if (confirmacao) {
+        const listaTarefas = document.getElementById('lista_de_tarefas')
+        listaTarefas.innerHTML = ''
+        ajustarAlturaContainer()
+        localStorage.removeItem('tarefas')
+    }
+}
+
+function ajustarAlturaContainer() {
+    let listaDeTarefas = document.getElementById("lista_de_tarefas")
+    let alturaLista = listaDeTarefas.clientHeight
+    let alturaContainer = alturaLista + 200
+    let container = document.querySelector(".container")
+    container.style.height = alturaContainer + "px"
+}
+
+function salvaLista() {
+    const listaTarefas = document.getElementById('lista_de_tarefas').innerHTML
+    localStorage.setItem('tarefas', listaTarefas)
+}
+
+function carregaLista() {
+    const listaSalva = localStorage.getItem('tarefas')
+    if (listaSalva) {
+        document.getElementById('lista_de_tarefas').innerHTML = listaSalva
+        ajustarAlturaContainer()
+    }
+}
