0afcd19b0c98d8c6040b9f00ebb0f0013935a889
GustavoNbs <nebesgustavo@gmail.com>
2024-06-28 18:59:44 -0300

    Detalhes

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,14 @@ fetch(`https://api.github.com/users/${username}/repos`)
         });
     })
     .catch(error => console.error('Erro:', error));
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+        
+        navLinks.forEach(link => {
+            link.addEventListener('click', function() {
+                navLinks.forEach(nav => nav.classList.remove('active'));
+                this.classList.add('active');
+            });
+        });
+    });
